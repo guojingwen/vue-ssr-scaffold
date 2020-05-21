85c16b8fffa7b523b81cde0c1230e1c23f92fac3
guojw <2315162186@qq.com>
2020-05-21 23:19:51 +0800

    部署

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    apiServer: 'http://127.0.0.1:7001/api', // 'http://api.warmplace.com',
+    apiServer: 'http://api.warmplace.cn/api', // 'http://api.warmplace.cn',
     authorizeAddress: 'http://vuessr.warmplace.cn/login',
     domain: 'vuessr.warmplace.cn',
     htmlHeadAttrConfig: require('./html-head-config')
