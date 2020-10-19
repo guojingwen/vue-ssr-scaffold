47b544d800a9bbb2676ac54427ad89b1d7bc0be8
guojw <2315162186@qq.com>
2020-10-19 16:24:37 +0800

    服务端渲染问题

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     apiServer: 'https://api.warmplace.cn:10443/api', // 'http://api.warmplace.cn',
-    serverApiServer: 'http://192.168.0.40:10012', // 内网不通
+    serverApiServer: 'http://192.168.0.40:10012/api', // 外网不通不知道为什么
     authorizeAddress: 'https://ssr.warmplace.cn:10443/login',
     domain: 'ssr.warmplace.cn:10443',
     htmlHeadAttrConfig: require('./html-head-config')
