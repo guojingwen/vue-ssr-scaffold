c6ed6a827652c70c488e5ac3c805dd109a10d323
guojw <2315162186@qq.com>
2020-11-03 23:29:25 +0800

    更换服务器

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-    apiServer: 'https://api.warmplace.cn:10443/api', // 'http://api.warmplace.cn',
-    serverApiServer: 'http://192.168.0.40:10012/api', // 外网不通不知道为什么
-    authorizeAddress: 'https://ssr.warmplace.cn:10443/login',
-    domain: 'ssr.warmplace.cn:10443',
+    apiServer: 'https://api.warmplace.cn/api', // 'http://api.warmplace.cn',
+    serverApiServer: 'http://172.19.102.36:10012/api', // 外网不通不知道为什么
+    authorizeAddress: 'https://ssr.warmplace.cn/login',
+    domain: 'ssr.warmplace.cn',
     htmlHeadAttrConfig: require('./html-head-config')
 }
